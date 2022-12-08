41ece58e98fb766de7b86e871d04a07e9237f403
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-12-08 16:21:06 +0300

    Add constants

---
+++ UIPicker/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  UIPicker
+//
+//  Created by Pavel on 8.12.22.
+//
+
+import Foundation
+
+enum Constants {
+    static let languages = [(language: "Russian", abbreviation: "ru"), (language: "English", abbreviation: "en"), (language: "Belarusian", abbreviation: "by")]
+    static let heightMultiplier = 0.05
+    static let widthMultiplier = 0.7
+    static let offset = 10
+    static let cornerRadius: CGFloat = 10
+}
