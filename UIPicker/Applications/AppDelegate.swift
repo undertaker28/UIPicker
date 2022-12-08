1e25acf48e1dbb00f53a3577db638987ecb72efd
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-12-08 11:05:37 +0300

    Delete Sotoryboard and add project structure

---
+++ UIPicker/Applications/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UIPicker
+//
+//  Created by Pavel on 8.12.22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
