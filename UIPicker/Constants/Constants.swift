a575dc0ae055215cb93653f6f14c542d16065315
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-12-08 19:22:11 +0300

    Make UI

--- UIPicker/Constants/Constants.swift
+++ UIPicker/Constants/Constants.swift
@@ -10,7 +10,12 @@ import Foundation
 enum Constants {
     static let languages = [(language: "Russian", abbreviation: "ru"), (language: "English", abbreviation: "en"), (language: "Belarusian", abbreviation: "by")]
     static let heightMultiplier = 0.05
-    static let widthMultiplier = 0.7
-    static let offset = 10
-    static let cornerRadius: CGFloat = 10
+    static let widthMultiplier = 0.9
+    static let offsetFromTop = 40
+    static let offset = 8
+    static let cornerRadius: CGFloat = 8
+    static let widthOfStackView = 140
+    static let heightOfStackView = 120
+    static let widthOfPicker = 160
+    static let widthOfLogo = 300
 }
---
+++ UIPicker/ViewController/MainViewController.swift
@@ -0,0 +1,155 @@
+//
+//  MainViewController.swift
+//  UIPicker
+//
+//  Created by Pavel on 8.12.22.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    private lazy var selectedLanguage = Constants.languages[0]
+    
+    private let logoImageView: UIImageView = {
+        let image = UIImage(named: "Logo")?.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView(image: image)
+        imageView.tintColor = UIColor(named: "ElementsColor")
+        return imageView
+    }()
+    
+    private let greetingTextLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Welcome 🤩"
+        label.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        label.textColor = UIColor(named: "TextColor")
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    private let languagePickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.tintColor = UIColor(named: "TextColor")
+        pickerView.setValue(UIColor(named: "ElementsColor"), forKey: "textColor")
+        return pickerView
+    }()
+    
+    private let lightModeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Light", for: .normal)
+        button.setTitleColor(UIColor(named: "TextColor"), for: .normal)
+        button.backgroundColor = UIColor(named: "ButtonColor")
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.clipsToBounds = true
+        button.tag = 0
+        button.addTarget(MainViewController.self, action: #selector(changeMode), for: .touchUpInside)
+        return button
+    }()
+    
+    private let darkModeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Dark", for: .normal)
+        button.setTitleColor(UIColor(named: "TextColor"), for: .normal)
+        button.backgroundColor = UIColor(named: "ButtonColor")
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.clipsToBounds = true
+        button.tag = 1
+        button.addTarget(MainViewController.self, action: #selector(changeMode), for: .touchUpInside)
+        return button
+    }()
+    
+    private let defaultModeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Default", for: .normal)
+        button.setTitleColor(UIColor(named: "TextColor"), for: .normal)
+        button.backgroundColor = UIColor(named: "ButtonColor")
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.clipsToBounds = true
+        button.tag = 2
+        button.addTarget(MainViewController.self, action: #selector(changeMode), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var buttonsStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [lightModeButton, darkModeButton, defaultModeButton])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .equalCentering
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+        addSubviews()
+        makeConstraints()
+        languagePickerView.delegate = self
+        languagePickerView.dataSource = self
+    }
+    
+    private func addSubviews() {
+        view.addSubview(logoImageView)
+        view.addSubview(greetingTextLabel)
+        view.addSubview(languagePickerView)
+        view.addSubview(buttonsStackView)
+    }
+    
+    private func makeConstraints() {
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(Constants.offsetFromTop)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(Constants.widthOfLogo)
+            $0.height.equalTo(logoImageView.snp.width)
+        }
+        
+        greetingTextLabel.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.top.equalTo(logoImageView.snp.bottom)
+            $0.centerX.equalToSuperview()
+        }
+        
+        languagePickerView.snp.makeConstraints {
+            $0.height.equalToSuperview().multipliedBy(Constants.heightMultiplier)
+            $0.top.equalTo(greetingTextLabel.snp.bottom).offset(Constants.offset)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(Constants.widthOfPicker)
+        }
+        
+        buttonsStackView.snp.makeConstraints {
+            $0.width.equalTo(Constants.widthOfStackView)
+            $0.height.equalTo(Constants.heightOfStackView)
+            $0.top.equalTo(languagePickerView.snp.bottom).offset(Constants.offset)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    @objc func changeMode() {
+        let stackView = self.buttonsStackView.subviews.first
+        stackView?.subviews.forEach({ button in
+            switch button.tag {
+            case 0:
+                overrideUserInterfaceStyle = .light
+            case 1:
+                overrideUserInterfaceStyle = .dark
+            default:
+                overrideUserInterfaceStyle = .unspecified
+            }
+        })
+    }
+}
+
+extension MainViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Constants.languages.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Constants.languages[row].language
+    }
+}
